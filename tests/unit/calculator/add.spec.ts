5879289fd95f698d391aeb481e49b9e3189f745a
LaFibreDuDev <yannick.kuhn@oclock.io>
2025-02-14 22:24:45 +0100

    test de ci/cd

--- tests/unit/calculator/add.spec.ts
+++ tests/unit/calculator/add.spec.ts
@@ -5,9 +5,9 @@ test.group('Calculator add', () => {
   test('example test', async ({ assert }) => {
     assert.equal(1, 1)
   }),
-    test('calculator should add 2 and 3 to be 5', async ({ assert }) => {
+    test('calculator should add 4 and 3 to be 7', async ({ assert }) => {
       const calculatorService = new CalculatorService()
-      const result = calculatorService.add(2, 3)
-      assert.equal(5, result)
+      const result = calculatorService.add(4, 3)
+      assert.equal(7, result)
     })
 })
