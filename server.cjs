a1614dfd1703b6de1edd12f227c7747118f02853
LaFibreDuDev <yannick.kuhn@oclock.io>
2025-03-12 21:04:53 +0100

    Mise à jour

--- server.cjs
+++ server.cjs
@@ -1,6 +1,30 @@
-import('./build/ace.js')
-  .then(({ default: ace }) => ace())
-  .catch((err) => {
-    console.error('Failed to run Ace CLI:', err)
-    process.exit(1)
-  })
+;(async () => {
+  await import('reflect-metadata')
+  const { Ignitor, prettyPrintError } = await import('@adonisjs/core')
+  const { pathToFileURL } = await import('url')
+  const path = await import('path')
+
+  const APP_ROOT = pathToFileURL(path.join(__dirname, '../')).href
+
+  const IMPORTER = async (filePath) => {
+    if (filePath.startsWith('./') || filePath.startsWith('../')) {
+      return import(new URL(filePath, APP_ROOT))
+    }
+    return import(filePath)
+  }
+
+  new Ignitor(APP_ROOT, { importer: IMPORTER })
+    .tap((app) => {
+      app.booting(async () => {
+        await import('#start/env')
+      })
+      app.listen('SIGTERM', () => app.terminate())
+      app.listenIf(app.managedByPm2, 'SIGINT', () => app.terminate())
+    })
+    .httpServer()
+    .start()
+    .catch((error) => {
+      process.exitCode = 1
+      prettyPrintError(error)
+    })
+})()
